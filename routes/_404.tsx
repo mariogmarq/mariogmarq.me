af52b225aefb5ed85fff73a9627e7e4098f88099
mariogmarq <mariogarciamarq@gmail.com>
2022-11-11 19:30:33 +0100

    new 404

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -2,8 +2,13 @@ import { UnknownPageProps } from "$fresh/server.ts";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
-    <div class="w-screen h-screen text-center">
-      <h1 class="font-black text-4xl block">404</h1>
+    <div class="w-screen h-screen text-center flex bg-[#EDE7DC]">
+      <div class="font-black text-8xl block my-auto mx-auto">
+        <a class="hover:text-underline" href="/about">
+          404
+        </a>
+        <p class="text-2xl text-gray-700">You seem quite lost</p>
+      </div>
     </div>
   );
 }
